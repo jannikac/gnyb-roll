5d49086a1d5ba47a13f03562cd5d79455c26cf1e
waffeln <sinusstudios@gmail.com>
2020-10-14 16:58:13 +0200

    paar optimierungen

--- random.js
+++ random.js
@@ -1,78 +1,31 @@
-//var content = document.getElementById("display");
-var button = document.getElementById("random-button");
-var nameBox = document.getElementById("name-box");
-const imageUrl = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/"
-var champInfos = {};
-var lastNumber = null;
+const button = document.getElementById("random-button")
+const nameBox = document.getElementById("name-box")
+const imageBox = document.getElementById("image-box")
 
-var globals = {
-    champs: 0,
-    version: 0,
-};
-const rollDelay = 100; //delay between roll animation
-var i = 0;
+const jsonFetch = url => fetch(url).then(r => r.json())
+const sleep = ms => new Promise(r => setTimeout(r, ms))
 
+;(async () => {
+  const allVersions = await jsonFetch("https://ddragon.leagueoflegends.com/api/versions.json")
+  const version = allVersions[0]
 
+  const champsJSON = await jsonFetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`)
 
+  const champs = champsJSON.data
 
-document.addEventListener('DOMContentLoaded', function() {
-    getVersion();
-}, false);
+  button.addEventListener("click", async () => {
+    let randomNumber
 
-function logConsole(responseData){
-    console.log(responseData)
-};
+    for (let i = 0; i < 10; i++) {
+      randomNumber = Math.floor(Math.random() * Object.values(champs).length)
+      button.textContent = randomNumber
+      await sleep(100)
+    }
 
-function setGlobals(index, responseData){
-    globals[index] = responseData;
-};
+    const champName = Object.keys(champs)[randomNumber]
+    const champ = champs[champName]
 
-
-function getVersion(){
-    $.getJSON('https://ddragon.leagueoflegends.com/api/versions.json', function(json) {
-        setGlobals("version", json[0]); //most recent version has index of 0
-        champsApiString = `https://ddragon.leagueoflegends.com/cdn/${json[0]}/data/en_US/champion.json`;
-        getChampInfos(champsApiString);
-    });
-};
-
-function getChampInfos(champsApiString){
-    $.getJSON(champsApiString, function(json) {
-        setGlobals("champs", Object.keys(json.data).length);
-        champInfos = json.data;
-    });
-};
-
-generateRandomNumber = function(){
-    return Math.floor(Math.random() * globals["champs"]);
-};
-displayNumber = function() {
-    lastNumber = generateRandomNumber();
-    button.textContent = lastNumber;
-};
-
-function displayName(lastNumber) {
-    nameBox.innerHTML = champInfos[Object.keys(champInfos)[lastNumber]].id;
-    imageString = `images/${champInfos[Object.keys(champInfos)[lastNumber]].key}.png`
-    document.getElementById("image-box").src = imageString;
-};
-
-button.onclick = function(){
-    roll();
-};
-
-function roll(){
-    setTimeout(function() {
-        displayNumber();
-        i++;
-        if (i < 10) {
-            roll();
-        }
-        else {
-            i = 0;
-            displayName(lastNumber);
-        }
-    }, rollDelay)
-};
-
-
+    nameBox.textContent = champ.id
+    imageBox.src = `images/${champ.key}.png`
+  })
+})()
